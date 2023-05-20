b8d6e5f20b3e485b57789d4db687b9bfad6f24c5
Andrew Zhu <xhinker@hotmail.com>
2023-05-20 01:02:51 -0700

    auto commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='diffusers_plus',
+    version='1.230520.1',
+    license='Apache License',
+    author="Andrew Zhu",
+    author_email='xhinker@hotmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/xhinker/diffusers_plus',
+    keywords='diffusers stable-diffusion',
+    install_requires=[
+        'diffusers'
+    ],
+)
