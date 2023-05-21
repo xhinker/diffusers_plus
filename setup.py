64a95eef875bbc7bedcfa886286a6742f01996e4
Andrew Zhu <xhinker@hotmail.com>
2023-05-21 00:13:39 -0700

    auto commit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup,find_packages
 
 setup(
     name='diffusers_plus',
-    version='1.230520.1',
+    version='1.230521.1',
     license='Apache License',
     author="Andrew Zhu",
     author_email='xhinker@hotmail.com',
